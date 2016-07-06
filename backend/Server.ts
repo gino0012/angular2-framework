06d63108038a520545b6aa7336db52c42e398230
Gino Saavedra <gino_0012@yahoo.com.ph>
2016-07-06 11:30:59 +0800

    add app use node_modules

--- backend/Server.ts
+++ backend/Server.ts
@@ -7,7 +7,7 @@ var helpers = require('../config/helpers');
 
 export class Server {
     private app: express.Express;
-    private port = process.env.PORT || 8080;
+    private port = process.env.PORT || 1234;
 
     constructor() {
         this.app = express();
