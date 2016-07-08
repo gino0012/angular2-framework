c53e6c4cfe4a9de460aa758fa557baecf792ac9f
Gino Carlo Saavedra <gino.saavedra@nokia.com>
2016-07-08 16:53:40 +0800

    fix error

--- backend/Routes.ts
+++ backend/Routes.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as path from 'path';
 
 export class Routes {
-    constructor(app, config) {
+    constructor(app:express.Express, config:any) {
         app.use(express.static(path.join(config.root + '/frontend')));
         app.use('/node_modules', express.static(path.join(config.root + '/node_modules')));
     }
